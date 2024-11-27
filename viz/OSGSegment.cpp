7614df309843adba673bcf97cee714a81a0dcf77
Christoph Hertzberg <christoph.hertzberg@dfki.de>
2024-11-27 11:04:59 +0100

    Adaptions for sdformat15 compatibility

--- viz/OSGSegment.cpp
+++ viz/OSGSegment.cpp
@@ -165,7 +165,7 @@ void OSGSegment::createInertias(std::vector<sdf::ElementPtr> const& inertia_arra
         //  <inertia ixx="0.00089755" ixy="1.4484e-08" ixz="-2.6127e-07" iyy="0.00057565" iyz="-7.8027e-07" izz="0.00056347"/>
         //</inertial>
         osg::PositionAttitudeTransform* to_cog = new osg::PositionAttitudeTransform();
-        ignition::math::Pose3d pose = elem->GetElement("pose")->Get<ignition::math::Pose3d>();
+        math::Pose3d pose = elem->GetElement("pose")->Get<math::Pose3d>();
         sdf_to_osg(pose , *to_cog);
         inertia_->addChild(to_cog);
 
@@ -311,7 +311,7 @@ osg::ref_ptr<osg::Group> OSGSegment::createVisual(urdf::VisualSharedPtr visual, 
 osg::ref_ptr<osg::Group> OSGSegment::createVisual(sdf::ElementPtr sdf_visual, QDir baseDir){
 
     osg::PositionAttitudeTransform* to_visual = new osg::PositionAttitudeTransform();
-    sdf_to_osg(sdf_visual->GetElement("pose")->Get<ignition::math::Pose3d>(), *to_visual);
+    sdf_to_osg(sdf_visual->GetElement("pose")->Get<math::Pose3d>(), *to_visual);
 
     osg::ref_ptr<osg::Group> root = osg::ref_ptr<osg::Group>(new osg::Group());
     osg::Node* osg_visual = 0;
@@ -325,7 +325,7 @@ osg::ref_ptr<osg::Group> OSGSegment::createVisual(sdf::ElementPtr sdf_visual, QD
         }
         else if (sdf_geom_elem->GetName() == "box"){
             osg::Vec3f size;
-            sdf_to_osg(sdf_geom_elem->GetElement("size")->Get<ignition::math::Vector3d>(), size);
+            sdf_to_osg(sdf_geom_elem->GetElement("size")->Get<math::Vector3d>(), size);
             osg::ShapeDrawable* drawable = new osg::ShapeDrawable(new osg::Box(osg::Vec3(0,0,0), size.x(), size.y(), size.z()));
             osg_visual = new osg::Geode;
             osg_visual->asGeode()->addDrawable(drawable);
@@ -345,7 +345,7 @@ osg::ref_ptr<osg::Group> OSGSegment::createVisual(sdf::ElementPtr sdf_visual, QD
         }
         else if (sdf_geom_elem->GetName() == "mesh") {
             osg::Vec3 scale;
-            sdf_to_osg(sdf_geom_elem->GetElement("scale")->Get<ignition::math::Vector3d>(), scale);
+            sdf_to_osg(sdf_geom_elem->GetElement("scale")->Get<math::Vector3d>(), scale);
 
             to_visual->setScale(scale);
 
